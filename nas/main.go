0372d7e332626b60cb186f0ee36d20a0567e1689
mkwcat <cat@mkw.re>
2024-01-10 08:11:38 -0500

    NAS: Limit HTTP request logging

--- nas/main.go
+++ nas/main.go
@@ -59,7 +59,6 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	logging.Info("NAS", aurora.Yellow(r.Method), aurora.Cyan(r.URL), "via", aurora.Cyan(r.Host), "from", aurora.BrightCyan(r.RemoteAddr))
 	moduleName := "NAS:" + r.RemoteAddr
 
 	// Handle conntest server
@@ -120,6 +119,7 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	logging.Info("NAS", aurora.Yellow(r.Method), aurora.Cyan(r.URL), "via", aurora.Cyan(r.Host), "from", aurora.BrightCyan(r.RemoteAddr))
 	replyHTTPError(w, 404, "404 Not Found")
 }
 
