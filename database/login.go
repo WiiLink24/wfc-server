e81702060ee3ad09ad933d95da961f24118ac9f3
Palapeli <26661008+mkwcat@users.noreply.github.com>
2026-04-06 01:42:24 -0400

    Database: Fix overly greedy ban search query

--- database/login.go
+++ database/login.go
@@ -12,27 +12,27 @@ import (
 )
 
 const (
-	SearchUserBan = `WITH known_ng_device_ids AS (
+	SearchUserBan = `
+	WITH known_ng_device_ids AS (
 		WITH RECURSIVE device_tree AS (
 			SELECT unnest(ng_device_id) AS device_id
-				FROM users
-				WHERE ng_device_id && $1
+			FROM users
+			WHERE allow_default_keys = FALSE AND ng_device_id && $1
 			UNION
 			SELECT unnest(ng_device_id)
-				FROM users
-				JOIN device_tree dt
-				ON ng_device_id && array[dt.device_id]
+			FROM users
+			JOIN device_tree dt
+			ON allow_default_keys = FALSE AND ng_device_id && array[dt.device_id]
 		) SELECT array_agg(DISTINCT device_id) FROM device_tree
-	)
-	SELECT has_ban, ban_tos, ng_device_id, ban_reason
-		FROM users
-		WHERE has_ban = true
-			AND ((profile_id = $2 OR allow_default_keys = FALSE)
-				OR ng_device_id && (SELECT * FROM known_ng_device_ids)
-				OR last_ip_address = $3
-				OR ($4 != '' AND last_ip_address = $4))
-			AND (ban_expires IS NULL OR ban_expires > $5)
-			ORDER BY ban_tos DESC LIMIT 1`
+	) SELECT has_ban, ban_tos, ng_device_id, ban_reason
+	FROM users
+	WHERE has_ban = true
+	  AND (profile_id = $2
+	  	OR (allow_default_keys = FALSE AND ng_device_id && (SELECT * FROM known_ng_device_ids))
+	  	OR last_ip_address = $3
+		OR ($4 != '' AND last_ip_address = $4))
+	  AND (ban_expires IS NULL OR ban_expires > $5)
+	ORDER BY ban_tos DESC LIMIT 1`
 )
 
 var (
